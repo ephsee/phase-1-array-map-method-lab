abf85e9001efe194f17fbf1f00a0f370e8cb1948
Francois Spies <fcspies@gmail.com>
2022-01-14 11:49:35 -0500

    commit tree c8434a01d2c181484c1cbbdc8f38c2fe80b542dd

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  const toCaps = tutorials.map((str) => str.charAt(0).toUpperCase() + str.slice(1))
+  return toCaps
 }
